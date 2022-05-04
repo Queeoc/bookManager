4de59896c64537d2046e966251d7e0ecfa11c619
victor-kai-xuan <66103914+victor-kai-xuan@users.noreply.github.com>
2022-05-04 11:04:31 +0800

    Update BookmanagerApplicationTests.java
    测试类首先将100000到100100的条形码存好在D:/bookBarcode

--- src/test/java/com/xkx/bookmanager/BookmanagerApplicationTests.java
+++ src/test/java/com/xkx/bookmanager/BookmanagerApplicationTests.java
@@ -1,9 +1,50 @@
 package com.xkx.bookmanager;
 
+import org.junit.jupiter.api.Test;
+import org.krysalis.barcode4j.impl.code128.Code128Bean;
+import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.StringUtils;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 @SpringBootTest
 class BookmanagerApplicationTests {
 
+    @Test
+    public void test(){
+        int count = 100000;
+        for(;count<100100;count++) {
+            String path = "D:/bookBarcode/" + count + ".jpg";
+            String msg = ""+count;
+            try {
+                File file = new File(path);
+                OutputStream ous = new FileOutputStream(file);
+                if (StringUtils.isEmpty(msg) || ous == null)
+                    return;
+                //选择条形码类型(好多类型可供选择)
+                Code128Bean bean = new Code128Bean();
+
+                //设置长宽
+                final double moduleWidth = 0.20;
+                final int resolution = 150;
+                bean.setModuleWidth(moduleWidth);
+                bean.doQuietZone(false);
+                String format = "image/png";
+                // 输出流
+                BitmapCanvasProvider canvas = new BitmapCanvasProvider(ous, format,
+                        resolution, BufferedImage.TYPE_BYTE_BINARY, false, 0);
+                //生成条码
+                bean.generateBarcode(canvas, msg);
+                canvas.finish();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
 }
